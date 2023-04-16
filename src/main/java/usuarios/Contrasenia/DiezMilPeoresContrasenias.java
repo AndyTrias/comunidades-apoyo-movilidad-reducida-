a8225e1d87d9134f7783e01daa74c44ae1edd628
manutorrente <manutorrente9@gmail.com>
2023-04-16 19:55:01 -0300

    leer el archivo agregado

--- src/main/java/usuarios/Contrasenia/DiezMilPeoresContrasenias.java
+++ src/main/java/usuarios/Contrasenia/DiezMilPeoresContrasenias.java
@@ -1,18 +1,34 @@
 package usuarios.Contrasenia;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-import java.util.Arrays;
 
 public class DiezMilPeoresContrasenias {
 
+    private static String path = "10000Peores.txt"; //ordenado para la busqueda
 
-    private static String[] lista;
 
-            // Sorteada para el binarySearch()
 
-    public static boolean estaEnLaLista(String contrasenia){
-        int resultado = Arrays.binarySearch(DiezMilPeoresContrasenias.lista, contrasenia);
-        return resultado < 0; //si es < 0, el resultado no esta en la lista
-
+    public static boolean estaEnLaLista(String contrasenia) {
+        try (BufferedReader br = new BufferedReader(new FileReader(DiezMilPeoresContrasenias.path))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                int cmp = line.compareTo(contrasenia);
+                if (cmp == 0) {
+                    // encontro la palabra
+                    return true;
+                } else if (cmp > 0) {
+                    // ya paso por el lugar en el que tendria que estar, por lo que se deja de buscar
+                    return false;
+                }
+            }
+            // termino el archivo y no la encontro todavia
+            return false;
+        } catch (IOException e) {
+            System.err.println("Error leyendo archivo: " + e.getMessage());
+            return false;
+        }
     }
 }
 
