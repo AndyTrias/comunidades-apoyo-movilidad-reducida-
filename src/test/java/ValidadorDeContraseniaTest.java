00ae72e38bcf3510ec88bd0e11f1f6c7a508fa5a
manutorrente <manutorrente9@gmail.com>
2023-04-16 23:01:12 -0300

    tests arreglados

--- src/test/java/ValidadorDeContraseniaTest.java
+++ src/test/java/ValidadorDeContraseniaTest.java
@@ -6,26 +6,37 @@ import static org.junit.Assert.*;
 public class ValidadorDeContraseniaTest {
 
     @Test
-    public void testValidarContrasenia() {
-
-        // Contrasenia válida
-        String contraseniaValida = "MiContrasenia123";
-        assertTrue(ValidadorDeContrasenia.validarContrasenia(contraseniaValida));
-
-        // Contrasenia con menos de 8 caracteres
-        String contraseniaCorta = "abc123";
-        assertFalse(ValidadorDeContrasenia.validarContrasenia(contraseniaCorta));
-
-        // Contrasenia con más de 64 caracteres
-        String contraseniaLarga = "EstaContraseniaTieneMasDeSesentaYCuatroCaracteresYDeberiaSerInvalida123";
-        assertFalse(ValidadorDeContrasenia.validarContrasenia(contraseniaLarga));
-
-        // Contrasenia que se encuentra en la lista de contraseñas comunes
-        String contraseniaComun = "12345678";
-        assertFalse(ValidadorDeContrasenia.validarContrasenia(contraseniaComun));
-
-        // Contrasenia que cumple algunas condiciones pero no todas
-        String contraseniaNoValida = "abcd123";
-        assertFalse(ValidadorDeContrasenia.validarContrasenia(contraseniaNoValida));
+    public void testContraseniaValida() {
+        String contrasenia = "MiContrasenia123";
+        assertTrue(ValidadorDeContrasenia.validarContrasenia(contrasenia));
     }
+    @Test
+    public void testContraseniaMenosDe8Caracteres() {
+        String contrasenia = "abc123";
+        assertFalse(ValidadorDeContrasenia.validarContrasenia(contrasenia));
+    }
+
+    @Test
+    public void testValidarContraseniaMasDe63Caracteres() {
+        String contrasenia = "EstaContraseniaTieneMasDeSesentaYCincoCaracteres1234";
+        assertFalse(ValidadorDeContrasenia.validarContrasenia(contrasenia));
+    }
+
+    @Test
+    public void testValidarContraseniaInsegura() {
+        String contrasenia = "password";
+        assertFalse(ValidadorDeContrasenia.validarContrasenia(contrasenia));
+    }
+
+    @Test
+    public void testValidarContraseniaVacia() {
+        String contrasenia = "";
+        assertFalse(ValidadorDeContrasenia.validarContrasenia(contrasenia));
+    }
+
+
+
+
+
+
 }
