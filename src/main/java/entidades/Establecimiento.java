25dce9f1bf45975892c99b8e9eca478f55597707
Andres Martin Trias <andrestrias03@gmail.com>
2023-06-06 10:02:38 -0300

    h

--- src/main/java/entidades/Establecimiento.java
+++ src/main/java/entidades/Establecimiento.java
@@ -2,12 +2,26 @@ package entidades;
 
 import comunidades.servicios.PrestacionDeServicio;
 import localizacion.Localizacion;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Establecimiento {
-    private String nombre;
-    private Localizacion localizacion;
-    private List<PrestacionDeServicio> servicios;
+    @Getter private String nombre;
+    @Getter @Setter private Localizacion localizacion;
+    @Getter private Set<PrestacionDeServicio> servicios;
 
+    public Establecimiento(String nombre, Localizacion localizacion) {
+        this.nombre = nombre;
+        this.localizacion = localizacion;
+        this.servicios = new HashSet<>();
+    }
+
+    public void agregarServicio(PrestacionDeServicio servicio) {
+        this.servicios.add(servicio);
+    }
 }
