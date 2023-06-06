099fd96110ae0eb92acbcf5dc701e108c4f025a3
Andres Martin Trias <andrestrias03@gmail.com>
2023-06-06 10:16:41 -0300

    set x lista

--- src/main/java/entidades/Entidad.java
+++ src/main/java/entidades/Entidad.java
@@ -5,17 +5,19 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 //comente cosas para probar lo del csv porque por ahora solo instancio en nombre de la clase
 public class Entidad {
-    private List<Establecimiento> establecimientos;
+    private Set<Establecimiento> establecimientos;
     @Setter private Localizacion localizacion;
     @Getter private String nombre;
 
     public Entidad(String nombre) {
         this.nombre = nombre;
-        this.establecimientos = new ArrayList<>();
+        this.establecimientos = new HashSet<>();
     }
 
     public void agregarEstablecimiento(Establecimiento establecimiento) {
