ed13535c2dc8174056b9a65ac404575599b0398f
Andres Martin Trias <andrestrias03@gmail.com>
2023-06-27 22:23:25 -0300

    ajuste adapter

--- src/main/java/notificaciones/whatsapp/AdapterWhatsapp.java
+++ src/main/java/notificaciones/whatsapp/AdapterWhatsapp.java
@@ -1,18 +1,30 @@
 package notificaciones.whatsapp;
 
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
 import comunidades.usuario.Usuario;
 import external.whatsapp.ServicioWhatsApp;
 import notificaciones.Notificacion;
 
 public class AdapterWhatsapp implements IAdapterWhatsapp{
 
-  private ServicioWhatsApp adaptadaAWhatsapp;
+  public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
+  public static final String AUTH_TOKEN = System.getenv("TWILIO_ACCOUNT_SID");
 
-  public AdapterWhatsapp(ServicioWhatsApp adaptadaAWhatsapp) {
-    this.adaptadaAWhatsapp = adaptadaAWhatsapp;
-  }
 
   public void notificar(Usuario usuario, Notificacion notificacion) {
-    adaptadaAWhatsapp.enviarWhatsapp(usuario.getTelefono(), notificacion.getMensaje());
+    final String telefono = String.format("whatsapp:%s", usuario.getTelefono());
+    final String mensaje = String.format("Hola %s, %s",  notificacion.getMensaje());
+
+    ServicioWhatsApp.enviarWhatsapp(telefono, mensaje);
   }
+
+
+  public static void enviarWhatsapp(String destinatario, String mensaje) {
+      Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+      Message message = Message.creator(new com.twilio.type.PhoneNumber(telefono),
+          new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
+          mensaje).create();
+    }
+
 }
