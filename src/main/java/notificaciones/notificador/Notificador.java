12a1b11829cba6149d2e06259fa653fc2856cd1f
fpesce27 <fpesce@frba.utn.edu.ar>
2023-07-09 17:56:00 -0300

    modified:   Entregables/DiagramaDeClases.wsd 	deleted:    src/main/java/notificaciones/notificador/AperturaIncidente.java 	deleted:    src/main/java/notificaciones/notificador/FactoryFormatoDeNotificacion.java 	modified:   src/main/java/notificaciones/notificador/Notificador.java 	deleted:    src/main/java/servicios/docs.txt 	deleted:    src/main/java/servicios/uml.txt 	new file:   src/test/java/RevisionDeIncidenteTest.java

--- src/main/java/notificaciones/notificador/Notificador.java
+++ src/main/java/notificaciones/notificador/Notificador.java
@@ -4,6 +4,9 @@ import comunidades.usuario.Usuario;
 import comunidades.usuario.configuraciones.formas.FormaDeRecibir;
 import notificaciones.Notificacion;
 
-public interface Notificador {
-    void notificar(Notificacion notificacion, FormaDeRecibir formaDeRecibir);
+public class AperturaIncidente implements Notificador{
+
+    public void notificar(Notificacion notificacion, FormaDeRecibir formaDeRecibir) {
+        formaDeRecibir.notificar(notificacion);
+    }
 }
