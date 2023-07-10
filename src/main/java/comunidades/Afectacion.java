e58a64d756d69e98a87e392e0396bd64c15aa8d8
Andres Martin Trias <andrestrias03@gmail.com>
2023-07-09 23:27:12 -0300

    afectado u observador

---
+++ src/main/java/comunidades/Afectacion.java
@@ -0,0 +1,15 @@
+package comunidades;
+
+import comunidades.servicios.PrestacionDeServicio;
+import lombok.Getter;
+import lombok.Setter;
+
+public class Afectacion {
+  @Getter @Setter private boolean afectado;
+  @Getter private PrestacionDeServicio prestacionDeServicio;
+
+  public Afectacion(PrestacionDeServicio prestacionDeServicio) {
+    this.prestacionDeServicio = prestacionDeServicio;
+    this.afectado = true;
+  }
+}
--- src/main/java/comunidades/Membresia.java
+++ src/main/java/comunidades/Membresia.java
@@ -1,14 +1,42 @@
 package comunidades;
 
+import comunidades.servicios.PrestacionDeServicio;
 import lombok.Getter;
 
-public class Membresia {
-    @Getter private Comunidad comunidad;
-    @Getter private Rol rol;
+import java.util.ArrayList;
+import java.util.List;
 
-    public Membresia(Comunidad comunidad, Rol rol) {
-        comunidad = comunidad;
-        rol = rol;
+public class Membresia {
+  @Getter private Comunidad comunidad;
+  @Getter private Rol rol;
+  @Getter private List<Afectacion> afectaciones;
+
+
+  public Membresia(Comunidad comunidad, Rol rol) {
+    this.comunidad = comunidad;
+    this.rol = rol;
+    this.afectaciones = new ArrayList<>();
+    this.afectacionesPorDefecto();
+  }
+
+  private void afectacionesPorDefecto() {
+    for (PrestacionDeServicio prestacion : this.comunidad.getServiciosDeInteres()) {
+      this.agregarAfectacion(prestacion, true);
     }
+  }
+
+  public void agregarAfectacion(PrestacionDeServicio prestacion, boolean afectado) {
+    Afectacion afectacion = new Afectacion(prestacion);
+    afectacion.setAfectado(afectado);
+    this.afectaciones.add(afectacion);
+  }
+
+  public void cambiarAfectacion(PrestacionDeServicio prestacion, Comunidad comunidad){
+    for (Afectacion afectacion : this.afectaciones) {
+      if (afectacion.getPrestacionDeServicio().equals(prestacion)) {
+        afectacion.setAfectado(!afectacion.isAfectado());
+      }
+    }
+  }
 
 }
