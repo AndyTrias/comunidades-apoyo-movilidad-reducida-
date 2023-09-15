4e5a5e1f93e087bbc83ce7d8a8f5d8c12c710cb6
federico perez <federperez@frba.utn.edu.ar>
2023-09-15 17:32:49 -0300

    test cascada

--- src/test/java/Persistencia/PersistenciaTest.java
+++ src/test/java/Persistencia/PersistenciaTest.java
@@ -102,4 +102,9 @@ public class PersistenciaTest implements SimplePersistenceTest {
         repoComunidad.modificar(comunidad);
     }
 
+    @Test
+    void borrarLocalizacion() { //cascada con ubicacion
+        Localizacion localizacion = repoLocalizacion.buscar(1L);
+        repoLocalizacion.eliminar(localizacion);
+    }
 }
