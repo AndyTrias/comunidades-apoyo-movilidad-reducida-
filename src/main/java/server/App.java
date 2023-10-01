010a532c07bc92c8100bd44852a0b9b1cdf69674
Andres Martin Trias <andrestrias03@gmail.com>
2023-09-30 21:27:42 -0300

    pude correr javalin

--- src/main/java/server/App.java
+++ src/main/java/server/App.java
@@ -1,6 +1,7 @@
 package server;
 
 public class App {
+
     public static void main(String[] args) {
         Server.init();
     }
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,8 +1,13 @@
 package server;
 
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
 import io.javalin.Javalin;
 import io.javalin.config.JavalinConfig;
+import io.javalin.http.HttpStatus;
+import io.javalin.rendering.JavalinRenderer;
 
+import java.io.IOException;
 import java.util.function.Consumer;
 
 public class Server {
@@ -16,9 +21,9 @@ public class Server {
 
   public static void init() {
     if(app == null) {
-      int port = Integer.parseInt(System.getProperty("port", "8080"));
+      Integer port = Integer.parseInt(System.getProperty("port", "8080"));
       app = Javalin.create(config()).start(port);
-//      initTemplateEngine();
+      initTemplateEngine();
       Router.init();
     }
   }
@@ -32,4 +37,22 @@ public class Server {
     };
   }
 
+
+  private static void initTemplateEngine() {
+    JavalinRenderer.register(
+        (path, model, context) -> { // Función que renderiza el template
+          Handlebars handlebars = new Handlebars();
+          Template template = null;
+          try {
+            template = handlebars.compile(
+                "templates/" + path.replace(".hbs",""));
+            return template.apply(model);
+          } catch (IOException e) {
+            e.printStackTrace();
+            context.status(HttpStatus.NOT_FOUND);
+            return "No se encuentra la página indicada...";
+          }
+        }, ".hbs" // Extensión del archivo de template
+    );
+  }
 }
