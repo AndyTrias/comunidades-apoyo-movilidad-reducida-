b730226c686575d8f366b45f4b6821b515fa211a
federperez <federperez@frba.utn.edu.ar>
2023-11-02 19:15:03 -0300

    controller de informe (ta rarito)

--- src/main/java/controllers/InformesController.java
+++ src/main/java/controllers/InformesController.java
@@ -1,9 +1,39 @@
 package controllers;
 
 import lombok.AllArgsConstructor;
+import models.comunidades.Comunidad;
+import models.entidades.Entidad;
+import models.rankings.criterios.CriteriosDeComunidades;
+import models.rankings.criterios.CriteriosDeEntidades;
+import models.rankings.informes.GeneradorDeInformes;
+
+import java.util.List;
 
 @AllArgsConstructor
 public class InformesController {
 
+//lo que tengo es el repositorio con los informes buscados por fecha
+    // necesito los datos para pasarle al generador de informes
+
+
+
+    public List<List<String>> generarInformes(List<CriteriosDeComunidades> criteriosComunidades, List<CriteriosDeEntidades> criteriosEntidades, List<Comunidad> comunidades, List<Entidad> entidades) {
+        GeneradorDeInformes generador = new GeneradorDeInformes();
+
+        // Agregar los criterios al generador
+        for (CriteriosDeComunidades criterio : criteriosComunidades) {
+            generador.agregarCriterioDeComunidad(criterio);
+        }
+
+        for (CriteriosDeEntidades criterio : criteriosEntidades) {
+            generador.agregarCriterioDeEntidad(criterio);
+        }
+
+        // Generar los datos del informe
+        List<List<String>> informes = generador.generarDatos(entidades, comunidades);
+
+        return informes;
+
+    }
 
 }
