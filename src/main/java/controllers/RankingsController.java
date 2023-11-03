9c32aad0ef9b3cada4eb73eedec915ba42bf48fb
Franco Pesce <83614061+fpesce27@users.noreply.github.com>
2023-11-03 17:01:38 +0000

    html seleccionarLocalidad

--- src/main/java/controllers/RankingsController.java
+++ src/main/java/controllers/RankingsController.java
@@ -1,4 +1,18 @@
 package controllers;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+/* main class */
 public class RankingsController {
+
+    /* main method */
+    public static void main(String[] args) {
+       JsonParser parser = new JsonParser();
+         String json = "{\"name\":\"Mahesh Kumar\", \"age\":21}";
+            JsonElement object = parser.parse(json);
+            System.out.println(object);
+    }
 }
+
+
