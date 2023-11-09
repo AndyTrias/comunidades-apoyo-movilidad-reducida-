3ecf38427113a2ba7f5f16a60ab23a83809b29b4
fpesce27 <fpesce@frba.utn.edu.ar>
2023-11-09 20:32:46 -0300

    crud intereses

---
+++ src/main/java/models/repositorios/RepoInteres.java
@@ -0,0 +1,9 @@
+package models.repositorios;
+
+import models.usuario.Interes;
+
+public class RepoInteres extends RepoGenerico<Interes> {
+    public RepoInteres() {
+        super(Interes.class);
+    }
+}
