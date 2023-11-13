9f9696e4eb38ccb09212e23d870e19b0449df766
manutorrente <manutorrente9@gmail.com>
2023-11-12 23:23:39 -0300

    services

--- src/python/incidentes/lista_incidentes/urls.py
+++ src/python/incidentes/lista_incidentes/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 import views
 
 urlpatterns = [
-    path("", views, name="lista_incidentes")
+    path("", views, name="lista_incidentes"),
+    path("incidente/<int:id>", views, name="incidente")
 ]
--- src/python/incidentes/lista_incidentes/views.py
+++ src/python/incidentes/lista_incidentes/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def lista_incidentes(request):
     return render(request, "incidentes.html", {})
+
+
+def incidente(request, id):
+    return render(request, "incidente.html", {})
